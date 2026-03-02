7f525092719298c5e58beb5d7fd4b757bffad436
sarvasva86 <suchipatel5486@gmail.com>
2026-03-02 13:31:26 -0500

    Update DB config to use DATABASE_URL for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,13 @@
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
+
+
